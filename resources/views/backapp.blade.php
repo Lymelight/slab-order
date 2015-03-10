df8b7b9b121d492716f735e561080c6972ae78ee
Chris Mills <xxchrismillsxx@gmail.com>
2015-03-10 18:53:13 -0400

    added compiled css files

--- resources/views/backapp.blade.php
+++ resources/views/backapp.blade.php
@@ -7,6 +7,7 @@
 	<title>Laravel</title>
 
 	<link href="/css/base.css" rel="stylesheet">
+    {{ URL::asset('css/base.css') }}
 
 	<!-- Fonts -->
 	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
