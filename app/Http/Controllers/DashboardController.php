86976f06c484abd5fe2dcd20abe3c23e26e78635
Chris Mills <xxchrismillsxx@gmail.com>
2015-03-10 19:08:37 -0400

    changed view returned

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -29,7 +29,7 @@ class DashboardController extends Controller {
 	 */
 	public function index()
 	{
-		return view('business\dashboard');
+		return view('business/dashboard');
 	}
 
 }
--- resources/views/backapp.blade.php
+++ resources/views/backapp.blade.php
@@ -7,7 +7,6 @@
 	<title>Laravel</title>
 
 	<link href="/css/base.css" rel="stylesheet">
-    {{ URL::asset('css/base.css') }}
 
 	<!-- Fonts -->
 	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
